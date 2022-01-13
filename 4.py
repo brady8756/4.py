f63d13ca002d413f2a7ec9bb914f7b1828a61935
brady8756 <95857599+brady8756@users.noreply.github.com>
2022-01-13 19:14:55 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,6 @@
 import random
+# import cv2
+# import numpy as np
 import streamlit as st
 
 class Die:
@@ -38,7 +40,15 @@ for idx, die in enumerate(st.session_state.dice):
     die.keep = st.checkbox(str(die.value), key=str(idx))
     if die.value != current_value:
         has_won = False
+        
 
 if has_won:
     st.write("你贏啦~!!")
     st.balloons()
+    '''
+    ## 謝謝來玩 請再次光臨!
+    ## 如果想再玩一次 請按F5!
+    ## 祝大家都可以all pass開心過寒假
+    '''
+if st.button('帥哥'):
+   st.image("https://scontent.xx.fbcdn.net/v/t1.15752-9/p206x206/271439506_408933894191183_71515324726547938_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=aee45a&_nc_ohc=SkRYdAoBUCYAX98dbDa&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIxzcU3KzZN4112mwDNgHjbSJC6eneygMfDYRYDymGJnQ&oe=6204F2D0")
